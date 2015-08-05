ef1efb647f1f6f9b6bf9ff785b9a2bd1e0037157
Viktor Kuchyn <kuchin.victor@gmail.com>
2015-08-05 12:09:13 +0700

    extracted game servie test to separate file

---
+++ app/gameplay/game_service_test.js
@@ -0,0 +1,95 @@
+/**
+ * Created by viktor on 8/5/15.
+ */
+
+describe('game service ', function () {
+
+    beforeEach(module('leela.gameplay'));
+
+    var $scope, GameService, $localStorage, board;
+
+    beforeEach(inject(function (_GameService_, _$localStorage_) {
+        GameService = _GameService_;
+        $localStorage = _$localStorage_;
+
+        $localStorage.game = undefined;
+        $localStorage.game_archive = undefined;
+        board = {last_cell: 72, cosmic_cell: 68, cells: {}};
+    }));
+
+    it('should generate new game', function () {
+        var newGame = GameService.createNewGame(123);
+        expect(newGame.deposit).toBe(0);
+        expect(newGame.born).toBe(false);
+        expect(newGame.finished).toBe(false);
+        expect(newGame.history).toEqual([]);
+        expect(newGame.current_position).toBe(123);
+    });
+
+    it('should undo last move for empty array', function () {
+        var game = GameService.createNewGame(68);
+        GameService.undoLastMove(game, board);
+        expect(game.history).toEqual([]);
+    });
+
+    it('should undo last move', function () {
+        var game = GameService.createNewGame(68);
+        game.history = [1, 2, 3];
+        GameService.undoLastMove(game, board);
+        expect(game.history).toEqual([1, 2]);
+    });
+
+    it('should undo first move', function () {
+        var game = GameService.createNewGame(6);
+        game.born = true;
+        game.history = [6];
+        GameService.undoLastMove(game, board);
+        expect(game.history).toEqual([]);
+        expect(game.born).toBe(false);
+        expect(game.current_position).toBe(68);
+    });
+
+    it('should set finished to false when game is finished', function () {
+        var game = GameService.createNewGame(68);
+        game.finished = true;
+        game.history = [1, 2];
+        GameService.undoLastMove(game, board);
+        expect(game.finished).toBe(false);
+    });
+
+    it('should undo current position on undo move', function () {
+        var game = GameService.createNewGame(68);
+        game.history = [6, 9];
+        GameService.undoLastMove(game, board);
+        expect(game.current_position).toBe(6);
+    });
+
+    it('should undo current position to arrow on undo move', function () {
+        var game = GameService.createNewGame(68);
+        game.history = [6, 10, 24];
+        board.cells = {10: {goto: 23}}
+        GameService.undoLastMove(game, board);
+        expect(game.current_position).toBe(23);
+    });
+
+    it('should archive game after new game was created', function () {
+        var oldGame = {born: true, finished: false, current_position: 10, history: [6]};
+        $localStorage.game = oldGame;
+        var game = GameService.createNewGame(68);
+        expect($localStorage.game).toEqual(game);
+        expect($localStorage.game_archive).toEqual([oldGame]);
+    });
+
+    it('should add another game to archive after new game was created', function () {
+        var game1 = {born: true, finished: false, current_position: 10, history: [6]};
+        $localStorage.game = game1;
+        var game2 = GameService.createNewGame(24);
+        GameService.createNewGame(25);
+        expect($localStorage.game_archive).toEqual([game1, game2]);
+    });
+
+    it('should load game from', function () {
+
+    });
+
+});
--- app/gameplay/gameplay_test.js
+++ app/gameplay/gameplay_test.js
@@ -2,193 +2,113 @@ describe('GameplayController', function () {
     beforeEach(module('leela.gameplay'));
     beforeEach(module('leela.board'));
 
-    var $controller, $scope, GameService, $localStorage;
+    var $controller, $scope;
 
-    beforeEach(inject(function (_$controller_, _GameService_, _$localStorage_) {
+    beforeEach(inject(function (_$controller_, _GameService_) {
         $controller = _$controller_;
-        GameService = _GameService_;
-        $localStorage = _$localStorage_;
 
         $scope = {};
         board = {last_cell: 72, cosmic_cell: 68, cells: {}};
         $controller('GameplayController', {$scope: $scope, board: board});
-        $localStorage.game = undefined;
-        $localStorage.game_archive = undefined;
-        $scope.game = GameService.createNewGame(6);
-        $scope.game.born = true;
+        $scope.game = {born: true, finished: false, deposit: 0, current_position: 6, history: []};
     }));
 
-    describe('$scope.moveChip', function () {
-
-        it('moves chip from the initial state to new position', function () {
-            $scope.moveChip(4);
-            expect($scope.game.current_position).toBe(10);
-        });
-
-        it('should thow error if step out of range [1-6]', function () {
-            expect(function () {
-                $scope.moveChip(0)
-            }).toThrow();
-            expect(function () {
-                $scope.moveChip(7)
-            }).toThrow();
-        });
-
-        it('should born only after 6', function () {
-            $scope.board.cells = {"6": {"title": "moha"}};
-            $scope.game.born = false;
-            $scope.game.current_position = 68;
-            $scope.moveChip(1);
-            $scope.moveChip(2);
-            $scope.moveChip(3);
-            $scope.moveChip(4);
-            $scope.moveChip(5);
-
-            $scope.moveChip(6);
-            expect($scope.game.current_position).toBe(6);
-        });
-
-        it('should add steps if 6', function () {
-            $scope.moveChip(6);
-            expect($scope.game.current_position).toBe(6);
-            expect($scope.game.deposit).toBe(6);
-            $scope.moveChip(6);
-            expect($scope.game.current_position).toBe(6);
-            expect($scope.game.deposit).toBe(12);
-            $scope.moveChip(1);
-            expect($scope.game.current_position).toBe(19);
-            expect($scope.game.deposit).toBe(0);
-        });
-
-        it('should reset deposit if 6x3', function () {
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            expect($scope.game.deposit).toBe(18);
-            $scope.moveChip(2);
-            expect($scope.game.current_position).toBe(8);
-        });
-
-        it('should reset deposit if 6x6', function () {
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            $scope.moveChip(6);
-            expect($scope.game.deposit).toBe(36);
-            $scope.moveChip(2);
-            expect($scope.game.current_position).toBe(8);
-        });
-
-        it('shouldn\'t move if out of board', function () {
-            $scope.game.current_position = 69;
-            $scope.moveChip(4);
-            expect($scope.game.current_position).toBe(69);
-            $scope.moveChip(2);
-            expect($scope.game.current_position).toBe(71);
-        });
-
-        it('should move from 10 to 23 by arrow', function () {
-            $scope.board.cells = {10: {"title": "tapah", goto: 23}};
-            $scope.moveChip(4);
-            expect($scope.game.current_position).toBe(23);
-        });
-
-        it('should move from 12 to 8 by snake', function () {
-            $scope.board.cells = {12: {title: "eirshya", goto: 8}};
-            $scope.game.current_position = 11;
-            $scope.moveChip(1);
-            expect($scope.game.current_position).toBe(8);
-        });
-
-        it('should finish game when gets on 68', function () {
-            $scope.game.current_position = 67;
-            $scope.moveChip(1);
-            expect($scope.game.current_position).toBe(68);
-            expect($scope.game.finished).toBe(true);
-        });
-
-        it('expect error when try to make a move after game is finished', function () {
-            $scope.game.finished = true;
-            expect(function () {
-                $scope.moveChip(1)
-            }).toThrow();
-        });
-
+    it('moves chip from the initial state to new position', function () {
+        $scope.moveChip(4);
+        expect($scope.game.current_position).toBe(10);
     });
 
-    describe('game service ', function () {
-        it('should generate new game', function () {
-            var newGame = GameService.createNewGame(123);
-            expect(newGame.deposit).toBe(0);
-            expect(newGame.born).toBe(false);
-            expect(newGame.finished).toBe(false);
-            expect(newGame.history).toEqual([]);
-            expect(newGame.current_position).toBe(123);
-        });
-
-        it('should undo last move for empty array', function () {
-            var game = GameService.createNewGame(68);
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.history).toEqual([]);
-        });
-
-        it('should undo last move', function () {
-            var game = GameService.createNewGame(68);
-            game.history = [1, 2, 3];
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.history).toEqual([1, 2]);
-        });
-
-        it('should undo first move', function () {
-            var game = GameService.createNewGame(6);
-            game.born = true;
-            game.history = [6];
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.history).toEqual([]);
-            expect(game.born).toBe(false);
-            expect(game.current_position).toBe(68);
-        });
-
-        it('should set finished to false when game is finished', function () {
-            var game = GameService.createNewGame(68);
-            game.finished = true;
-            game.history = [1, 2];
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.finished).toBe(false);
-        });
-
-        it('should undo current position on undo move', function () {
-            var game = GameService.createNewGame(68);
-            game.history = [6, 9];
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.current_position).toBe(6);
-        });
-
-        it('should undo current position to arrow on undo move', function () {
-            var game = GameService.createNewGame(68);
-            game.history = [6, 10, 24];
-            $scope.board.cells = {10: {goto: 23}}
-            GameService.undoLastMove(game, $scope.board);
-            expect(game.current_position).toBe(23);
-        });
-
-        it('should archive game after new game was created', function () {
-            $localStorage.game = $scope.game;
-            var game = GameService.createNewGame(68);
-            expect($localStorage.game).toEqual(game);
-            expect($localStorage.game_archive).toEqual([$scope.game]);
-        });
-
-        it('should add another game to archive after new game was created', function () {
-            var game1 = $scope.game;
-            var game2 = GameService.createNewGame(24);
-            GameService.createNewGame(25);
-            expect($localStorage.game_archive).toEqual([game1, game2]);
-        });
-
+    it('should thow error if step out of range [1-6]', function () {
+        expect(function () {
+            $scope.moveChip(0)
+        }).toThrow();
+        expect(function () {
+            $scope.moveChip(7)
+        }).toThrow();
     });
+
+    it('should born only after 6', function () {
+        $scope.board.cells = {"6": {"title": "moha"}};
+        $scope.game.born = false;
+        $scope.game.current_position = 68;
+        $scope.moveChip(1);
+        $scope.moveChip(2);
+        $scope.moveChip(3);
+        $scope.moveChip(4);
+        $scope.moveChip(5);
+
+        $scope.moveChip(6);
+        expect($scope.game.current_position).toBe(6);
+    });
+
+    it('should add steps if 6', function () {
+        $scope.moveChip(6);
+        expect($scope.game.current_position).toBe(6);
+        expect($scope.game.deposit).toBe(6);
+        $scope.moveChip(6);
+        expect($scope.game.current_position).toBe(6);
+        expect($scope.game.deposit).toBe(12);
+        $scope.moveChip(1);
+        expect($scope.game.current_position).toBe(19);
+        expect($scope.game.deposit).toBe(0);
+    });
+
+    it('should reset deposit if 6x3', function () {
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        expect($scope.game.deposit).toBe(18);
+        $scope.moveChip(2);
+        expect($scope.game.current_position).toBe(8);
+    });
+
+    it('should reset deposit if 6x6', function () {
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        $scope.moveChip(6);
+        expect($scope.game.deposit).toBe(36);
+        $scope.moveChip(2);
+        expect($scope.game.current_position).toBe(8);
+    });
+
+    it('shouldn\'t move if out of board', function () {
+        $scope.game.current_position = 69;
+        $scope.moveChip(4);
+        expect($scope.game.current_position).toBe(69);
+        $scope.moveChip(2);
+        expect($scope.game.current_position).toBe(71);
+    });
+
+    it('should move from 10 to 23 by arrow', function () {
+        $scope.board.cells = {10: {"title": "tapah", goto: 23}};
+        $scope.moveChip(4);
+        expect($scope.game.current_position).toBe(23);
+    });
+
+    it('should move from 12 to 8 by snake', function () {
+        $scope.board.cells = {12: {title: "eirshya", goto: 8}};
+        $scope.game.current_position = 11;
+        $scope.moveChip(1);
+        expect($scope.game.current_position).toBe(8);
+    });
+
+    it('should finish game when gets on 68', function () {
+        $scope.game.current_position = 67;
+        $scope.moveChip(1);
+        expect($scope.game.current_position).toBe(68);
+        expect($scope.game.finished).toBe(true);
+    });
+
+    it('expect error when try to make a move after game is finished', function () {
+        $scope.game.finished = true;
+        expect(function () {
+            $scope.moveChip(1)
+        }).toThrow();
+    });
+
 
     describe('$scope.showHistory', function () {
         it('store previous position after move in history', function () {
@@ -215,4 +135,5 @@ describe('GameplayController', function () {
             expect($scope.showHistory()).toEqual([12, 9]);
         });
     })
+
 });
